1a8d0dc5e1947b0ef5a4f2bb9490015ac3447306
Chung JooYoung <hyundia1947@naver.com>
2024-02-06 17:57:34 +0900

    dept list tbl 생성

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import InsertMemo from "./component/InsertMemo";
 import MemoList from "./component/MemoList";
+import DepartmentList from "./component_jy/DepartmentList";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <InsertMemo />
-        <hr />
         <Routes>
-          <Route path="/" element={<MemoList />} />
+          <Route path="/jy" element={<DepartmentList />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/main/java/com/magic4/magicard/test/DepartmentRepo.java
@@ -0,0 +1,11 @@
+package com.magic4.magicard.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.magic4.magicard.vo.Department;
+
+public interface DepartmentRepo extends JpaRepository <Department, Integer>{
+
+    
+    
+}
